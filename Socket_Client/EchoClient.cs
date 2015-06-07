94f331b25db82d1f0285533fb5f8f3fff9efdcb0
AlbenZz <834150509@qq.com>
2015-06-07 23:16:45 +0800

    Initial Commit
    add EchoClient

---
+++ Socket_Client/EchoClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Socket_Client
+{
+    class EchoClient
+    {
+
+        private Socket client_Socket;
+
+        EchoClient() {
+            
+
+        }
+
+        public Int32 IPv4Addr { get; set; }
+        public Int16 TCP_Port { get; set; }
+
+
+
+        
+        public void SetupSocket() { }
+
+        public void InitiateConnection()
+        {
+
+        }
+
+        public void SendEchoString(string echo) 
+        { 
+        }
+
+    }
+}
